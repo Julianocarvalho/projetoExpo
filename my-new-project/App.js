8ad5d0a4c4757419a593458404a6cafdd03b6e0a
Juliano Carvalho <julianocarvalho128@gmail.com>
2019-02-28 17:34:04 -0300

    first commit

---
+++ my-new-project/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! Loucura em teste...  </Text>
+        <TextInput 
+          style={styles.inputStyle}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputStyle: {
+    height: 40,
+    borderColor: "green",
+    borderWidth: 1
+  }
+});
